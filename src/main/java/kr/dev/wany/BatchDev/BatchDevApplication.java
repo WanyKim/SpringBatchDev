b687efc228f04e42143784bd6185cf5adc4e9413
WanyKim <ktwkill@gmail.com>
2023-02-09 23:34:46 +0900

    SpringBatch 개발

---
+++ src/main/java/kr/dev/wany/BatchDev/BatchDevApplication.java
@@ -0,0 +1,13 @@
+package kr.dev.wany.BatchDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/dev/wany/BatchDev/BatchDevApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.dev.wany.BatchDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
