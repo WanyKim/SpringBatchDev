9a728a36f4f0e2ce15f3ba4b444b71e27b085e99
twkim <twkim@kcp.co.kr>
2025-04-17 13:30:10 +0900

    twkim_1

---
+++ src/main/java/kr/dev/wany/BatchDev/service/Test.java
@@ -0,0 +1,15 @@
+package kr.dev.wany.BatchDev.service;
+
+/************************************
+ * Name : Test
+ * To-Do : 
+ * Developer : twkim
+ * Date : 2025-04-17 오후 1:29
+ ************************************/
+public class Test {
+    public static void main(String[] args) {
+        /**
+         * feature/twkim_1
+         */
+    }
+}
