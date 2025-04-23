b364de57b42cd6b301dabecb83d547ea2b74345e
twkim <twkim@kcp.co.kr>
2025-04-23 15:27:39 +0900

    Revert "feature/alpha_1"
    This reverts commit 5982c4d8507d4e2ca8cd1944484fa822541e6069.

--- src/main/java/kr/dev/wany/test/alpha_1.java
+++
@@ -1,9 +0,0 @@
-package kr.dev.wany.test;
-
-/************************************
- * Name : alpha_1
- * To-Do : 
- * Developer : twkim
- * Date : 2025-04-23 오후 3:12
- ************************************/
-public class alpha_1 {}
