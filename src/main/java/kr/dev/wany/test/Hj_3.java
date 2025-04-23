7ea3822cef65433daf406eb8ff07b9edab9b82d1
twkim <twkim@kcp.co.kr>
2025-04-23 14:03:01 +0900

    Revert "feature/Tw_4"
    This reverts commit de482d126aaf0f0aa1c6225d6dffc2e2aacb2e65.

--- src/main/java/kr/dev/wany/test/Hj_3.java
+++
@@ -1,9 +0,0 @@
-package kr.dev.wany.test;
-
-/************************************
- * Name : Hj_3
- * To-Do : 
- * Developer : twkim
- * Date : 2025-04-23 오후 1:56
- ************************************/
-public class Hj_3 {}
