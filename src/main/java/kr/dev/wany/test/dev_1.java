9db48ac60709d9de910965aa405986df83f6fde1
twkim <twkim@kcp.co.kr>
2025-04-23 15:19:02 +0900

    feature/alpha_1

---
+++ src/main/java/kr/dev/wany/test/dev_1.java
@@ -0,0 +1,9 @@
+package kr.dev.wany.test;
+
+/************************************
+ * Name : dev_1
+ * To-Do : 
+ * Developer : twkim
+ * Date : 2025-04-23 오후 3:16
+ ************************************/
+public class dev_1 {}
