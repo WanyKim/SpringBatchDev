abe911dfea52b15a4b9f7f72e8c0d8e57941c6c4
twkim <twkim@kcp.co.kr>
2025-04-23 13:07:00 +0900

    test/twkim_6

---
+++ src/main/java/kr/dev/wany/test/Ms.java
@@ -0,0 +1,9 @@
+package kr.dev.wany.test;
+
+/************************************
+ * Name : Ms
+ * To-Do : 
+ * Developer : twkim
+ * Date : 2025-04-23 오후 1:06
+ ************************************/
+public class Ms {}
