dd38974770c81dac5b93b2a6b60f9bc82243c138
twkim <twkim@kcp.co.kr>
2025-04-23 15:31:01 +0900

    feature/alpha_2

---
+++ src/main/java/kr/dev/wany/test/Alpha2.java
@@ -0,0 +1,9 @@
+package kr.dev.wany.test;
+
+/************************************
+ * Name : Alpha2
+ * To-Do : 
+ * Developer : twkim
+ * Date : 2025-04-23 오후 3:30
+ ************************************/
+public class Alpha2 {}
