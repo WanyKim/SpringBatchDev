2c5a93ba5a778b88ab8e72dbfd4abb91e3117213
twkim <twkim@kcp.co.kr>
2025-04-24 18:59:43 +0900

    feature/Twkim_test_2

---
+++ src/main/java/kr/dev/wany/test/Twkim_test_2.java
@@ -0,0 +1,9 @@
+package kr.dev.wany.test;
+
+/************************************
+ * Name : Twkim_test_2
+ * To-Do : 
+ * Developer : twkim
+ * Date : 2025-04-24 오후 6:59
+ ************************************/
+public class Twkim_test_2 {}
