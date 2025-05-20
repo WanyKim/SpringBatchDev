1624a5fcf73c37fa5c1c0ac0096c3c9c7bdb6bc3
twkim <twkim@kcp.co.kr>
2025-05-20 16:39:21 +0900

    pull request test

--- src/main/java/kr/dev/wany/test/Test.java
+++ src/main/java/kr/dev/wany/test/Test.java
@@ -1,5 +1,10 @@
 package kr.dev.wany.test;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 /************************************
  * Name : Test
  * To-Do : 
@@ -11,4 +16,9 @@ public class Test {
      * Git Test : feature/twkim_2
      *
      */
+
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println( Date.from(now.truncatedTo( ChronoUnit.DAYS).atZone( ZoneId.systemDefault()).toInstant()) );
+    }
 }
